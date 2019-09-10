efcfe817ae3d436396a1d7a05bda8bcb52af7fa8
Weiting Tan <wtan12@jhu.edu>
2019-09-10 13:35:28 -0400

    initialization of hw1

---
+++ canvas.js
@@ -0,0 +1,9 @@
+function initialization() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
+}
+
+initialization();
