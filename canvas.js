6042edb9a8487edeb5c154c2044880f1888137b1
Weiting Tan <wtan12@jhu.edu>
2019-09-10 14:07:43 -0400

    finish board, need to add funcitons for placing pieces

--- canvas.js
+++ canvas.js
@@ -1,9 +1,19 @@
 function initialization() {
     var c = document.getElementById("myCanvas");
+    var width = 800;
+    var height = 800;
+    console.log(width);
     var ctx = c.getContext("2d");
-    ctx.moveTo(0,0);
-    ctx.lineTo(200,100);
-    ctx.stroke();
+    for (var i = 0; i <= 10; i++){
+        cx = i * width / 10;
+        cy = i * height / 10;
+        ctx.moveTo(0,cy);
+        ctx.lineTo(width,cy);
+        ctx.stroke();
+        ctx.moveTo(cx,0);
+        ctx.lineTo(cx,height);
+        ctx.stroke();
+    }
 }
 
 initialization();
